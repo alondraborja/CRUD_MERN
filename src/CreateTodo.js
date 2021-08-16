7f77734c5ef384711b64e96146aa5135f4993b18
Alondra Borja <alborja14.ab@gmail.com>
2021-08-16 19:20:47 +0800

    updated CreateTodo.js, EditTodo.js and TodoList.js

--- src/CreateTodo.js
+++ src/CreateTodo.js
@@ -1,5 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { TodoForm } from './TodoForm';
 
-export const CreateTodo = () => {
-    return <div className="container">Create Todo Item</div>
+export const CreateTodo = () => {  
+    const onSubmit = (data) => {
+        alert(JSON.stringify(data));
+    }; 
+    
+    return (
+        <div className="container">
+            <div className="mt-3">
+                <h3>Create Todo Item</h3>
+                <TodoForm onSubmit={onSubmit}/>
+            </div>
+        </div>
+    );
 }
